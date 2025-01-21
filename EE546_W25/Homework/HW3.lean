d79e7922d755184425f79a6538ccc90394741066
Eric Klavins <klavins@uw.edu>
2025-01-20 21:14:21 -0800

    hw3, prob 4

--- EE546_W25/Homework/HW3.lean
+++ EE546_W25/Homework/HW3.lean
@@ -46,6 +46,37 @@ example : ∀ p : Person, ∃ q : Person, next_to p q :=
 example : ∀ p : Person, ∃ q : Person, ¬next_to p q :=
   sorry
 
--- 4 UNDER CONSTRUCTION
+/- 4) Besides ∀ and ∃ that are other quantifiers we can define. For example, the "Exists Exactly One" quantifer allows you to state that there is only one of something. We usually written ∃! as in
+
+    ∃! x, P x
+
+which states there is exactly one x such that P x is true.
+
+We can define this quantifer inductively, just as we did for Exists: -/
+
+inductive Exists1 {α : Type} (p : α → Prop) : Prop where
+  | intro (x : α) (h : p x ∧ ∀ y : α, p y → x = y) : Exists1 p
+
+/- However, it is a pain to define the notation E!. So we will just have to write
+
+    Exists1 (λ x => P x)
+
+instead of the above. -/
+
+-- a) Prove the elimination theorem for Exists1
+
+theorem Exists1.elim {α : Type} {P : α → Prop} {b : Prop}
+   (h₁ : Exists1 (λ x => P x)) (h₂ : ∀ (a : α), P a → b) : b := sorry
+
+-- b) Prove the following examples:
+
+example : ∀ x, Exists1 (λ y : Person => ¬next_to y x ) := sorry
+
+example (α : Type) (P : α → Prop) : Exists1 ( λ x => P x ) → ¬ ∀ x, ¬ P x  := sorry
+
+example : Exists1 (λ x => x=0) := sorry
+
+example : ¬Exists1 (λ x => x ≠ 0) := sorry
+
 
 -- 5 UNDER CONSTRUCTION
