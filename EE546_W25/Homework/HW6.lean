4bcd78351a9d30116796996e2d2b78e930ec4665
Eric Klavins <klavins@uw.edu>
2025-02-15 10:39:10 -0800

    h26

--- EE546_W25/Homework/HW6.lean
+++ EE546_W25/Homework/HW6.lean
@@ -2,12 +2,13 @@
 
 Prepare a project update for Thursday Feb 20.
 
-If you are stuck with on complicated theorem and do not know where to go next, consider going back to simpler problems. For example,
+If you are stuck with on a complicated theorem and do not know where to go next, consider going back to simpler problems. For example,
 
   - If you are working with matrices, can you limit your results to 2x2 matrices?
+  - If you are working with infinite sums of matrices, can you work with infinite sums of real numbers?
   - If you are working with integrals, can you figure out how they are defined in Lean and show some examples? Can you do some basic manipulations with sums instead of integrals?
   - If you are trying to show an algorithm terminates, can you show that a simpler one terminates? Can you find examples of terminating algorithms by searching for "termination_by" and "decreasing_by" in the mathlib source code?
 
-Please plan to present everything in VSCode, with explanations in comments, the screen properly magnified, and without displaying any extraneously sub-windows.
+Please plan to present everything in VSCode, with explanations in comments, your windows properly magnified, and without displaying any extraneously sub-windows.
 
 -/
